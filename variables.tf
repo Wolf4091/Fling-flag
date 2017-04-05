d01e210e539da312605b2aaf85cfffe1eb9424b4
Wolf4091 <Wolf4091@gmail.com>
2017-04-04 21:08:34 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "aws_region" {
 
 variable "access_key" {
   description = "The AWS acess key."
-  default     = “access_key”
+  default     = “$(aws_access_key)"
 }
 
 variable "secret_key" {
   description = "The AWS secret."
-  default     = “secret_key”
+  default     = “$(aws_secret_key)"
 }
 
 variable "aws_amis" {
